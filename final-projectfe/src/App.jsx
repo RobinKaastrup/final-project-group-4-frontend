57b4d47b6a8503ef947c99bcd163b9e756f2d1d2
Robin Kaastrup <robin.kaastrup@no.experis.com>
2024-03-14 15:45:46 +0100

    added some stuff to the component tree

---
+++ final-projectfe/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return(
+    <div className="app-container">
+      <div className="menu-container">
+        hello
+      </div>
+      <div className="sub-menu-container">
+  
+
+      </div>
+      <div className="main-container">
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
