c7611a8154e5f1a5b660c23fa443c4db640c6353
Robin Kaastrup <robin.kaastrup@no.experis.com>
2024-03-18 14:36:15 +0100

    message added routing, signup and login

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='final-project-group-4-frontend'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
