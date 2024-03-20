6bd792eb98277da17878adc50a5c5d07d0a2a6c8
Robin Kaastrup <robin.kaastrup@no.experis.com>
2024-03-20 13:14:28 +0100

    added fetching and rendering of contacts

--- src/components/dashboard/DashboardContacts.jsx
+++ src/components/dashboard/DashboardContacts.jsx
@@ -1,26 +1,42 @@
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
+import axios from 'axios'
 import ContactElement from "../contactElement/ContactElement"
 
 function DashboardContacts() {
   const userId = useParams()
-  const contacts = useState([])
+  const [contacts, setContacts] = useState([])
   
   const getContacts = () => {
-    
+
   }
 
   useEffect(() => {
-
+    const token = localStorage.getItem('token')
+    axios
+      .get(`http://localhost:4000/relations/all`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then((response) => {
+        console.log(response.data)
+        const contactsList = response.data.map(relation => relation.friend)
+        setContacts(contactsList)
+        console.log("contacts loaded")
+      })
+      .catch(error => console.error(`Error: could not fetch contacts: `, error))
   }, [])
 
   return (
     <div className="contacts-dashboard">
       <div className="contacts-view">
         <ul className="contacts-list">
-          <ContactElement />
-          <ContactElement />
-
+          {console.log((contacts !== undefined ? contacts : "none"))}
+          {contacts !== undefined && contacts.map((contact, index) => (
+            <ContactElement key={index}/>  
+          ))}
+          
         </ul>
 
       </div>
---
+++ src/components/logo/Logo.jsx
@@ -0,0 +1,7 @@
+function Logo() {
+  return (
+    <svg></svg>
+  )
+}
+
+export default Logo
