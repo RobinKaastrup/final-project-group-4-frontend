2995112f34a040622e84871b094dd2f9e337e8aa
Petter Karstensen <petterkarstensen@gmail.com>
2024-03-21 09:44:49 +0100

    shows profile based on id

--- src/components/profile/DashboardProfile.jsx
+++ src/components/profile/DashboardProfile.jsx
@@ -1,23 +1,62 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { DataContext } from "../../App";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import './profile.css';
+import axios from "axios";
 
 function DashboardProfile() {
   const context = useContext(DataContext)
   const userId = useParams()
   const navigate = useNavigate();
+  const [currentProfile, setCurrentProfile] = useState({
+    username: '',
+    email: ''
+  });
+  const [canEdit, setCanEdit] = useState(false)
+
+  useEffect(() => {
+    console.log(userId.id)
+    console.log(context.loggedInUser.id)
+    if (userId.id === context.loggedInUser.id) {
+      setCanEdit(true);
+      setCurrentProfile({
+        username: context.loggedInUser.username,
+        email: context.loggedInUser.email
+      });
+    } else {
+      const authToken = localStorage.getItem("token");
+      axios.get(`${context.baseURL}/users/${userId.id}`, {
+        headers: {
+          Authorization: `Bearer ${authToken}`
+      }
+    })
+      .then(response => {
+        console.log(response)
+        const { username, email } = response.data;
+        setCurrentProfile({
+          username,
+          email
+        });
+      })
+      .catch(error => console.error('Error fetching user data:', error));
+    setCanEdit(false);
+    }
+  }, [userId, context]);
+
+
 
   return (
     <div className="profile-container">
       <div className="header"></div>
-       <div className="profile-info">
-        <h1 className="username">{context.loggedInUser.username}</h1>
-        <p className="email">{context.loggedInUser.email}</p>
-        <button className="edit-button" onClick={() => navigate(`/profile/edit/${context.loggedInUser.id}`)}>Edit Profile</button>
+      <div className="profile-info">
+        <h1 className="username">{currentProfile.username}</h1>
+        <p className="email">{currentProfile.email}</p>
+        {canEdit && (
+          <button className="edit-button" onClick={() => navigate(`/profile/edit/${context.loggedInUser.id}`)}>Edit Profile</button>
+        )}
       </div>
     </div>
-  )
+  );
 }
 //<img src={profilePictureUrl} alt="Profile Picture" className="profile-picture"/>
 
